5a7ec710dc6a9bd956141664873dc36142a5bfeb
paulkore <paulkore@outlook.com>
2015-02-04 17:23:36 -0500

    Disable admin site access

--- django_sandbox/urls.py
+++ django_sandbox/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_sandbox.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-
+urlpatterns = patterns(
+    '',
     url(r'^$', 'django_sandbox.views.sandbox', name='root'),
     url(r'^sandbox$', 'django_sandbox.views.sandbox', name='sandbox'),
-
-
 )
