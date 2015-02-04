2b40167ed0a4d3317b7731edbdd3d682fa4a1f27
paulkore <paulkore@outlook.com>
2015-02-04 15:10:23 -0500

    Heroku configuration
    This is the first attempt to get this project to work on Heroku

--- django_sandbox/settings.py
+++ django_sandbox/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# [Heroku]:
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,12 +59,21 @@ WSGI_APPLICATION = 'django_sandbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    # [Heroku] Parse database configuration from $DATABASE_URL
+    'default': dj_database_url.config()
 }
+
+# [Heroku] Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -77,10 +89,27 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+# STATIC_URL = '/static/'
 
+# [Heroku]:
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# [Heroku]:
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
